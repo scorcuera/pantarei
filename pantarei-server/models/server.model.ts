b22bdd370f28b08ff97f948a4579851b44f7f71f
scorcuera <scorcuera6@gmail.com>
2024-02-24 18:41:32 +0100

    Add Server model

---
+++ pantarei-server/models/server.model.ts
@@ -0,0 +1,26 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+
+export class Server {
+    private port: number;
+    private app: Application;
+
+    constructor(port: number) {
+        this.port = port;
+        this.app = express();
+        this.settings();
+    }
+
+    settings() {
+        this.app.set("port", this.port || process.env.PORT || 3000);
+        this.app.use(express.json());
+        this.app.use(cors());
+    }
+
+    listen() {
+        this.app.listen(this.app.get("port"), () => {
+            console.log(`Server listening on port ${this.app.get("port")}`);
+        })
+    }
+
+}
