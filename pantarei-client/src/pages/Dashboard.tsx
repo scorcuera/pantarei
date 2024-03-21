f63fcca486dc6aaf88795ba8bfb486c7d0703a0c
scorcuera <scorcuera6@gmail.com>
2024-03-21 09:51:43 +0100

    Add user name to Dashboard

--- pantarei-client/src/pages/Dashboard.tsx
+++ pantarei-client/src/pages/Dashboard.tsx
@@ -1,7 +1,14 @@
+import AuthContext from "../context/AuthProvider"
+import { useContext } from "react";
+
 const Dashboard = () => {
+    const { auth } = useContext(AuthContext);
+    const userData = auth.data.user;
+    console.log(auth.data.user);
   return (
     <div>
-        <h1>This is the user Dashboard</h1>
+        <h1>Welcome {userData.name}</h1>
+        <h2>This is your Dashboard</h2>
     </div>
   )
 }
